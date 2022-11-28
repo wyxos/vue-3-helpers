9ff14872afc2d8e2f966a769207e5f7b4da3eccf
Joey J <joey.jaulimsing@live.com>
2022-11-28 14:18:54 +0400

    fix: externals + removed bulma

--- demo/src/main.js
+++ demo/src/main.js
@@ -2,10 +2,9 @@ import { createApp } from 'vue'
 import './style.scss'
 import App from './App.vue'
 import Oruga from '@oruga-ui/oruga-next'
-import { bulmaConfig } from '@oruga-ui/theme-bulma'
 
 const app = createApp(App)
 
-app.use(Oruga, { ...bulmaConfig, iconPack: 'fas' })
+app.use(Oruga, { iconPack: 'fas' })
 
 app.mount('#app')
--- demo/vite.config.js
+++ demo/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import json from '../package.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -20,13 +21,11 @@ export default defineConfig({
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ['vue', '@oruga-ui/oruga-next', '@oruga-ui/theme-bulma', 'axios', 'moment'],
+      external: [json.dependencies],
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps
-        globals: {
-          vue: 'Vue'
-        }
+        globals: { }
       }
     }
   }
