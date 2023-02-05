045aabea74f807dbbdd1875be5ef5eb545cf1252
Joey J <joey.jaulimsing@live.com>
2023-02-05 19:17:54 +0400

    feat: added loaded event

--- src/WyxosImage.vue
+++ src/WyxosImage.vue
@@ -1,65 +1,70 @@
 <script>
 export default {
-  name: 'WyxosImage',
-  props: {
-    src: {
-      type: [File, String],
-      required: true
+    name: 'WyxosImage',
+    props: {
+        src: {
+            type: [File, String],
+            required: true
+        },
+        resize: {
+            type: Object,
+            default: null
+        }
     },
-    resize: {
-      type: Object,
-      default: null
-    }
-  },
-  data() {
-    return {
-      width: 0,
-      height: 0
-    }
-  },
-  mounted() {
-    this.loadImage()
-  },
-  methods: {
-    loadImage() {
-      if (typeof this.src === 'object') {
-        return this.loadFile()
-      }
-
-      return this.loadPath()
+    emits: ['loaded'],
+    data () {
+        return {
+            width: 0,
+            height: 0
+        }
     },
-    loadFile() {
-      const reader = new FileReader()
-
-      reader.onload = (event) => {
-        const image = event.target.result
-
-        this.$refs.image.src = image
-
-        this.width = this.resize ? this.resize.width : image.width
-
-        this.height = this.resize ? this.resize.height : image.height
-      }
-
-      reader.readAsDataURL(this.src)
+    mounted () {
+        this.loadImage()
     },
-    loadPath() {
-      const image = new Image()
+    methods: {
+        loadImage () {
+            if (typeof this.src === 'object') {
+                return this.loadFile()
+            }
 
-      image.onload = () => {
-        this.$refs.image.src = this.src
+            return this.loadPath()
+        },
+        loadFile () {
+            const reader = new FileReader()
 
-        this.width = this.resize ? this.resize.width : image.width
+            reader.onload = (event) => {
+                const image = event.target.result
 
-        this.height = this.resize ? this.resize.height : image.height
-      }
+                this.$refs.image.src = image
 
-      image.src = this.src
+                this.width = this.resize ? this.resize.width : image.width
+
+                this.height = this.resize ? this.resize.height : image.height
+
+                this.$emit('loaded')
+            }
+
+            reader.readAsDataURL(this.src)
+        },
+        loadPath () {
+            const image = new Image()
+
+            image.onload = () => {
+                this.$refs.image.src = this.src
+
+                this.width = this.resize ? this.resize.width : image.width
+
+                this.height = this.resize ? this.resize.height : image.height
+
+                this.$emit('loaded')
+            }
+
+            image.src = this.src
+        }
     }
-  }
 }
 </script>
 
 <template>
-    <img ref="image" src="" alt="" :width="width" :height="height" />
+    <img ref="image" src="" alt="" :width="width" :height="height"/>
 </template>
